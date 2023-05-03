f1ddda1e4014df96185af32b0a9da0e7d98e9783
aligbakhtiyar <ahmadbakhtiyar@gmail.com>
2023-05-03 16:21:04 +0530

    First commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./components/Login";
+import Header from "./components/Header";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
