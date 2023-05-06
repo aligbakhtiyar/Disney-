ecbab3dd79266543f0a13710c7475498fd6cfff5
aligbakhtiyar <ahmadbakhtiyar@gmail.com>
2023-05-06 13:57:40 +0530

    login system

--- src/app/store.js
+++ src/app/store.js
@@ -1,15 +1,34 @@
-import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
+import { configureStore } from "@reduxjs/toolkit";
 import userReducer from '../features/user/userSlice'
 
-const store = require('../store/app')
-state = store.default.getState()
+// const store = require('../store/app')
+// state = store.default.getState()
 
  
-export default configureStore({
-    reducer:{
-        user: userReducer,
-    },
-    middleware: getDefaultMiddleware({
-        serializableCheck: false,
-    })
-}) 
+// export default configureStore({
+//     reducer:{
+//         user: userReducer,
+//     },
+//     middleware: getDefaultMiddleware({
+//         serializableCheck: false,
+//     })
+// }) 
+
+
+const reducer = {
+    user: userReducer,
+};
+
+const store = configureStore({
+    reducer: reducer,
+    devTools: true,
+})
+
+export default store;
+
+
+
+
+
+
+
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,70 +1,89 @@
 import styled from "styled-components";
 import { useDispatch, useSelector } from "react-redux";
-import {useNavigate} from 'react-router-dom'
-import {auth, provider} from '../firebase';
-import { selectUserName, selectUserEmail, selectUserPhoto, setUserLoginDetails } from "../features/user/userSlice";
+import { useNavigate } from "react-router-dom";
+import { auth, provider } from "../firebase";
+import {
+  selectUserName,
+  selectUserPhoto,
+  setUserLoginDetails,
+} from "../features/user/userSlice";
 
 const Header = (props) => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
-  const username = useSelector(selectUserName);
+  const userName = useSelector(selectUserName);
   const userPhoto = useSelector(selectUserPhoto);
-   const handleAuth = () => {
-    auth.signInWithPopup(provider).then((result) => {
-      setUser(result.user);
-    }).catch((error) => {
-      alert(error.message)
-    })
-   }
+  const handleAuth = () => {
+    auth
+      .signInWithPopup(provider)
+      .then((result) => {
+        console.log(result);
+        setUser(result.user);
+      })
+      .catch((error) => {
+        alert(error.message);
+      });
+  };
 
-   const setUser = (user) => {
+  const setUser = (user) => {
     dispatch(
       setUserLoginDetails({
         name: user.displayName,
         email: user.email,
         photo: user.photoURL,
       })
-    )
-   }
+    );
+  };
 
   return (
     <Nav>
       <Logo>
         <img src="images/logo.svg"></img>
       </Logo>
-      <NavMenu>
-        <a href="/home">
-          <img src="/images/home-icon.svg" alt="HOME" />
-          <span>Home</span>
-        </a>
 
-        <a href="/home">
-          <img src="/images/search-icon.svg" alt="SEARCH" />
-          <span>Search</span>
-        </a>
+      {!userName ? (
+        <Login onClick={handleAuth}>Login</Login>
+      ) : (
+        <>
+          <NavMenu>
+            <a href="/home">
+              <img src="/images/home-icon.svg" alt="HOME" />
+              <span>Home</span>
+            </a>
 
-        <a href="/home">
-          <img src="/images/watchlist-icon.svg" alt="WATCHLIST" />
-          <span>Watchlist</span>
-        </a>
+            <a href="/home">
+              <img src="/images/search-icon.svg" alt="SEARCH" />
+              <span>Search</span>
+            </a>
 
-        <a href="/home">
-          <img src="/images/home-icon.svg" alt="ORIGINALS" />
-          <span>Originals</span>
-        </a>
+            <a href="/home">
+              <img src="/images/watchlist-icon.svg" alt="WATCHLIST" />
+              <span>Watchlist</span>
+            </a>
 
-        <a href="/home">
-          <img src="/images/home-icon.svg" alt="MOVIES" />
-          <span>Movies</span>
-        </a>
+            <a href="/home">
+              <img src="/images/home-icon.svg" alt="ORIGINALS" />
+              <span>Originals</span>
+            </a>
 
-        <a href="/home">
-          <img src="/images/home-icon.svg" alt="SERIES" />
-          <span>Series</span>
-        </a>
-      </NavMenu>
-      <Login onClick={handleAuth
-      }>Login</Login>
+            <a href="/home">
+              <img src="/images/home-icon.svg" alt="MOVIES" />
+              <span>Movies</span>
+            </a>
+
+            <a href="/home">
+              <img src="/images/home-icon.svg" alt="SERIES" />
+              <span>Series</span>
+            </a>
+          </NavMenu>
+          <SignOut>
+            <UserImg src={userPhoto} alt={userName} />
+            <DropDown>
+              <span onClick={handleAuth}>Sign out</span>
+            </DropDown>
+          </SignOut>
+        </>
+      )}
     </Nav>
   );
 };
@@ -162,19 +181,61 @@ const NavMenu = styled.div`
   }
   `;
 
-
 const Login = styled.a`
-background-color : rgba(0,0,0,0.6);
-padding: 8px 16px;
-text-decoration: upparcase;
-letter-spacing: 1.5px;
-border:1px solid #f9f9f9;
-border-radius: 4px;
-transition: all 0.2s ease 0s;
+  background-color: rgba(0, 0, 0, 0.6);
+  padding: 8px 16px;
+  text-decoration: upparcase;
+  letter-spacing: 1.5px;
+  border: 1px solid #f9f9f9;
+  border-radius: 4px;
+  transition: all 0.2s ease 0s;
 
-&: hover{
-  background-color: #f9f9f9;
-  color: #000;
-}
+  &: hover {
+    background-color: #f9f9f9;
+    color: #000;
+  }
 `;
+
+const UserImg = styled.img`
+  height: 100%;
+`;
+
+const DropDown = styled.div`
+  position: absolute;
+  top: 48px;
+  right: 0px;
+  background: rgb(19, 19, 19);
+  border: 1px solid rgba(151, 151, 151, 0.34);
+  border-radius: 4px;
+  box-shadow: rgb(0 0 0 / 50%) 0px 0px 18px 0px;
+  padding: 10px;
+  font-size: 14px;
+  letter-spacing: 3px;
+  width: 100px;
+  opacity: 0;
+`;
+
+const SignOut = styled.div`
+  position: relative;
+  height: 48px;
+  width: 48px;
+  display: flex;
+  cursor: pointer;
+  align-items: center;
+  justify-content: center;
+
+  ${UserImg} {
+    border-radius: 50%;
+    width: 100%;
+    height: 100%;
+  }
+
+  &:hover {
+    ${DropDown} {
+      opacity: 1;
+      transition-duration: 1s;
+    }
+  }
+`;
+
 export default Header;
