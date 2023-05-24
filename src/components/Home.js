cb6a9d8f51a52c3c4a66fa3579fe9ee8ba535784
aligbakhtiyar <ahmadbakhtiyar@gmail.com>
2023-05-24 21:39:35 +0530

    imageslider

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,25 +1,35 @@
 import styled from "styled-components";
+import ImgSlider from "./ImgSlider";
 
 
 
 const Home = (props) => {
   return (
     <Container>
-        <h2>hxha</h2>
-        <h2>hxha</h2>
-        <h2>hxha</h2>
-        <h2>hxha</h2>
-        <h2>hxha</h2>
-        <h2>hxha</h2>
-        <h2>hxha</h2>
-        <h2>hxha</h2>
+        <ImgSlider/>
+      
     </Container>
   )
 }
 
 const Container = styled.main`
 position: relative;
-background: url("/images/home-background.png")
+mid-height: calc(100% - 250px);
+over-flow-x: hidden;
+top: 72px;
+margin-left: 20px
+padding: 0 calc(3.5vw -5px)
+
+&:after{
+  background: url("/images/home-background.png")
+  no-repeat fixed;
+  content: "";
+  inset: 0px;
+  opacity:1;
+  z-index: -1;
+
+}
+
 `;
 
 export default Home
